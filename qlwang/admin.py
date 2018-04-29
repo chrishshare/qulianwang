e745a6b0c2d0883c236b589434a1057337b79345
chrishshare <chrishshare@163.com>
2018-04-30 00:19:12 +0800

    chushihua

--- qlwang/admin.py
+++ qlwang/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from qlwang.models import CollectModel, MyCollectModel, DictItemModel, StaticModel
+from qlwang.models import CollectModel, MyCollectModel, DictItemModel, StaticModel, WXUser
 
 
 admin.site.register(DictItemModel)
 admin.site.register(CollectModel)
 admin.site.register(MyCollectModel)
 admin.site.register(StaticModel)
+admin.site.register(WXUser)
